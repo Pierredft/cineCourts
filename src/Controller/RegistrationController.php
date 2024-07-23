b23bf856e674c8d6bad0d685776bbb977abdc4ff
Nori Kedjam <nkedjam@gmail.com>
2024-07-23 11:58:44 +0200

    save registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,7 +34,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('home.index');
+            return $this->redirectToRoute('home.html.twig');
         }
 
         return $this->render('registration/register.html.twig', [
