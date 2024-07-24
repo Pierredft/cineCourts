22056be0dfbddf0acd3ac3bcb038ac97dad08620
Margaux Kowalczyk <m.kowalczyk.pro@gmail.com>
2024-07-24 09:04:53 +0200

    update

--- src/Controller/Admin/ActeursCrudController.php
+++ src/Controller/Admin/ActeursCrudController.php
@@ -3,10 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Acteurs;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ActeursCrudController extends AbstractCrudController
 {
