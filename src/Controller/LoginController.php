fd0e66b89c40ce41f2fb0fceb0d8c70947cbf717
Margaux Kowalczyk <m.kowalczyk.pro@gmail.com>
2024-07-24 14:04:09 +0200

    ajout login google

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,35 +3,61 @@
 namespace App\Controller;
 
 use App\Form\LoginFormType;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use KnpU\OAuth2ClientBundle\Client\Provider\OAuth2ClientInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
+{
+
+    public const SCOPES = [
+        'google' => [],
+    ];
+
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        #[Route('/login', name: 'app_login')]
-        public function login(AuthenticationUtils $authenticationUtils): Response
-        {
-            // get the login error if there is one
-            $error = $authenticationUtils->getLastAuthenticationError();
-            // last username entered by the user
-            $lastUsername = $authenticationUtils->getLastUsername();
-    
-            // $form = $this->createForm(LoginFormType::class, [
-            //     'username' => $lastUsername,
-            // ]);
-    
-            return $this->render('login/login.html.twig', [
-                'lastUsername' => $lastUsername,
-                'error' => $error,
-            ]);
-        }
-    
-        #[Route('/logout', name: 'app_logout')]
-        public function logout(): void
-        {
-            // controller can be blank: it will never be executed!
-            throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-        }
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // $form = $this->createForm(LoginFormType::class, [
+        //     'username' => $lastUsername,
+        // ]);
+
+        return $this->render('login/login.html.twig', [
+            'lastUsername' => $lastUsername,
+            'error' => $error,
+        ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // controller can be blank: it will never be executed!
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route("/oauth/connect/{service}", name: 'auth_oauth_connect', methods: ['GET'])]
+    public function connect(string $service, ClientRegistry $clientRegistry): RedirectResponse
+    {
+        if (! in_array($service, array_keys(array: self::SCOPES), true)) {
+            throw $this->createNotFoundException();
+        }
+
+        return $clientRegistry
+            ->getClient($service)
+            ->redirect(self::SCOPES[$service]);
+    }
+
+    #[Route('/oauth/check/{service}', name: 'check', methods: ['GET', 'POST'])]
+    public function check(): Response
+    {
+        return new Response('', 200);
+    }
+}
---
+++ src/Security/GoogleAuthenticator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Security\AbstractOAuthAuthenticator;
+use League\OAuth2\Client\Provider\GoogleUser;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+
+
+
+class GoogleAuthenticator extends AbstractOAuthAuthenticator
+{
+    protected string $serviceName = 'google'; 
+
+    protected function getUserFromResourceOwner(ResourceOwnerInterface $resourceOwner, UserRepository $repository): ?User
+    {
+        if (!($resourceOwner instanceof GoogleUser)) {
+            throw new \RuntimeException(message: 'expecting google user');
+        }
+
+        if (true != ($resourceOwner->toArray()['email_verified'] ?? null )) {
+            throw new AuthenticationException(message: 'email not verified');
+        }
+
+        return $repository->findOneBy([
+            'google_id' => $resourceOwner->getId(),
+            'email' => $resourceOwner->getEmail()
+        ]);
+    }
+}
