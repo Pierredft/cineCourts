a99bd38c6b053ac4fa5e9a7e627085e3bea41ae3
Margaux Kowalczyk <m.kowalczyk.pro@gmail.com>
2024-07-31 09:53:24 +0200

    modif du login twig

--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -1,4 +1,5 @@
 <?php
+// src/Form/LoginFormType.php
 
 namespace App\Form;
 
@@ -6,19 +7,17 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LoginFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class)
-            ->add('password', PasswordType::class);
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([]);
+            ->add('_username', TextType::class, [
+                'label' => 'Email :',
+            ])
+            ->add('_password', PasswordType::class, [
+                'label' => 'Mot de passe :',
+            ]);
     }
 }
