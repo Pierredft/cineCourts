40094ff0ffd31668e59b98e8a88a8422ffcb7e2b
pierre defauquet <pierredefauquet@gmail.com>
2024-08-21 14:47:37 +0200

    maj ajout de modif sur le home

--- src/Controller/Admin/GenreCrudController.php
+++ src/Controller/Admin/GenreCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Genre;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class GenreCrudController extends AbstractCrudController
 {
@@ -15,14 +17,16 @@ class GenreCrudController extends AbstractCrudController
         return Genre::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            ImageField::new('image')
+            ->setBasePath('images/')
+            ->setUploadDir('public/images/genre')
+            ->setRequired($pageName === Crud::PAGE_EDIT)
+            ->setFormTypeOptions($pageName === Crud::PAGE_EDIT ? ['allow_delete' => false] : []),
         ];
     }
-    */
 }
--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -19,6 +19,9 @@ class Genre
     #[ORM\ManyToOne(inversedBy: 'genre')]
     private ?Films $films = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -51,5 +54,17 @@ class Genre
     public function __toString(): string
     {
         return $this->nom;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
     }
 }
