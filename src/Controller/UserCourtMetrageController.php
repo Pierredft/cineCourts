87e0c6e5d25ab091679e5a33f2da1078f1e310c0
Nori Kedjam <nkedjam@gmail.com>
2024-08-26 10:49:20 +0200

    AJOUT SERVICE MAIL

--- src/Controller/UserCourtMetrageController.php
+++ src/Controller/UserCourtMetrageController.php
@@ -9,11 +9,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class UserCourtMetrageController extends AbstractController
 {
     #[Route('/soumettre-film', name: 'submitFilm')]
-    public function soumettre(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
+    public function soumettre(Request $request, EntityManagerInterface $em, SluggerInterface $slugger, MailerInterface $mailer): Response
     {
         $courtMetrage = new UserCourtMetrage();
         $formulaire = $this->createForm(UserCourtMetrageFormType::class, $courtMetrage);
@@ -35,6 +37,10 @@ class UserCourtMetrageController extends AbstractController
                     );
                 } catch (\Exception $e) {
                     // Gérer l'erreur si l'upload échoue
+                    $this->addFlash('error', 'Erreur lors du téléchargement du fichier.');
+                    return $this->render('films/submitFilm.html.twig', [
+                        'formulaire' => $formulaire->createView(),
+                    ]);
                 }
 
                 $courtMetrage->setNomFichierVideo($nouveauNom);
@@ -45,6 +51,36 @@ class UserCourtMetrageController extends AbstractController
             $em->persist($courtMetrage);
             $em->flush();
 
+            // Envoyer un e-mail à l'administrateur
+            $emailAdmin = (new Email())
+                ->from('votre_adresse_email@gmail.com')
+                ->to('hackathon.cinecourts@gmail.com') // Remplacez par l'adresse e-mail de l'administrateur
+                ->subject('Nouvelle soumission de court métrage')
+                ->text(
+                    "Un nouveau court métrage a été soumis.\n\n".
+                    "Titre: " . $courtMetrage->getTitre() . "\n".
+                    "Description: " . $courtMetrage->getDescription() . "\n".
+                    "Email: " . $courtMetrage->getEmail() . "\n".
+                    "Nom du fichier vidéo: " . $courtMetrage->getNomFichierVideo()
+                );
+
+            $mailer->send($emailAdmin);
+
+            // Envoyer un e-mail de confirmation à l'utilisateur
+            $emailUser = (new Email())
+                ->from('votre_adresse_email@gmail.com')
+                ->to($courtMetrage->getEmail()) // L'adresse e-mail de l'utilisateur soumettant le formulaire
+                ->subject('Confirmation de soumission')
+                ->text(
+                    "Merci d'avoir soumis votre court métrage.\n\n".
+                    "Titre: " . $courtMetrage->getTitre() . "\n".
+                    "Description: " . $courtMetrage->getDescription() . "\n".
+                    "Nous reviendrons vers vous bientôt."
+                );
+
+            $mailer->send($emailUser);
+
+            // Rediriger vers la page de profil
             return $this->redirectToRoute('app_profil');
         }
 
