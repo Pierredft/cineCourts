64aa7ee8e3dbe4e2628af4fb9044127297a6773e
pierre defauquet <pierredefauquet@gmail.com>
2024-08-27 21:20:32 +0200

    maj ajout de la vo dans l'entité film

--- src/Entity/Films.php
+++ src/Entity/Films.php
@@ -67,6 +67,9 @@ class Films
     #[ORM\OneToMany(targetEntity: Subtitle::class, mappedBy: 'films')]
     private Collection $subtitle;
 
+    #[ORM\Column(length: 255)]
+    private ?string $VO = null;
+
 
     public function __construct()
     {
@@ -299,6 +302,18 @@ class Films
                 $subtitle->setFilms(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getVO(): ?string
+    {
+        return $this->VO;
+    }
+
+    public function setVO(string $VO): static
+    {
+        $this->VO = $VO;
 
         return $this;
     }
